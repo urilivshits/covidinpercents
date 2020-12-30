7c925920ce767bea74bf9ff306204a3251588fbf
Uri Livshits <uri.livshits@gmail.com>
2020-12-30 17:21:29 +0200

    Updates

---
+++ static/js/xlsx.c0ba0790.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcovidinpercents=this.webpackJsonpcovidinpercents||[]).push([[2],{275:function(n,c){},363:function(n,c){},364:function(n,c){}}]);
+//# sourceMappingURL=xlsx.c0ba0790.chunk.js.map
