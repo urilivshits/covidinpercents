147e40ee6351e1098e2c25c0cd7699e341f65cb4
Uri Livshits <uri.livshits@gmail.com>
2021-01-12 19:19:11 +0200

    Updates

---
+++ static/js/xlsx.5a8fcabb.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcovidinpercents=this.webpackJsonpcovidinpercents||[]).push([[2],{276:function(n,c){},364:function(n,c){},365:function(n,c){}}]);
+//# sourceMappingURL=xlsx.5a8fcabb.chunk.js.map
--- static/js/xlsx.c0ba0790.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcovidinpercents=this.webpackJsonpcovidinpercents||[]).push([[2],{275:function(n,c){},363:function(n,c){},364:function(n,c){}}]);
-//# sourceMappingURL=xlsx.c0ba0790.chunk.js.map
